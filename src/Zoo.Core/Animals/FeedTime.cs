06e3f2ea4cee7cf90b5d82c86e3427f7ddafc5d4
Alexander Slesarenko <dragongling@gmail.com>
2021-11-12 15:26:14 +0500

    feat: Added feed time

---
+++ src/Zoo.Core/Animals/FeedTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo.Core.Animals
+{
+    public class FeedTime
+    {
+        public FeedTime(DateTime time)
+        {
+            Time = time;
+        }
+
+        public DateTime Time { get; private set; }
+    }
+}
