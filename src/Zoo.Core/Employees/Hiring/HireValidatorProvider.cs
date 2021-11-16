f168527f218a170e6cde2787aeda32bd5a351523
Alexander Slesarenko <dragongling@gmail.com>
2021-11-16 10:26:24 +0500

    feat: added `HireValidatorProvider`

---
+++ src/Zoo.Core/Employees/Hiring/HireValidatorProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Zoo.Core.Employees.Hiring
+{
+    public class HireValidatorProvider
+    {
+        public IHireValidator GetHireValidator(IEmployee employee)
+        {
+            return employee.GetType().Name switch
+            {
+                nameof(ZooKeeper) => new ZooKeeperValidator(),
+                nameof(Veterinarian) => new VeterinarianValidator(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ tests/Zoo.Core.Tests/Employees/Hiring/HireValidatorProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Zoo.Core.Employees.Hiring.Tests
+{
+    public class HireValidatorProviderTests
+    {
+        [Fact]
+        public void ShouldReturnRightValidator()
+        {
+            var hireValidatorProvider = new HireValidatorProvider();
+            Assert.Equal(nameof(ZooKeeperValidator), hireValidatorProvider.GetHireValidator(new ZooKeeper("L", "F")).GetType().Name);
+            Assert.Equal(nameof(VeterinarianValidator), hireValidatorProvider.GetHireValidator(new Veterinarian("L", "F")).GetType().Name);
+        }
+
+        [Fact]
+        public void ShouldReturnNullOnUknown()
+        {
+            var hireValidatorProvider = new HireValidatorProvider();
+            Assert.Null(hireValidatorProvider.GetHireValidator(new UnknownEmployee()));
+        }
+
+        private class UnknownEmployee : IEmployee
+        {
+            public string FirstName => "F";
+            public string LastName => "L";
+        }
+    }
+}
