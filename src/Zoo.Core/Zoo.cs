0f77a72f5326ff5b1966b13a77be7b634f13a9a2
Alexander Slesarenko <dragongling@gmail.com>
2021-11-16 11:08:06 +0500

    feat: enclosure implemented

---
+++ src/Zoo.Core/Zoo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Zoo.Core.Employees;
+
+namespace Zoo.Core
+{
+    public class Zoo
+    {
+        public List<Enclosure> Enclosures { get; private set; }
+        public List<IEmployee> Employees { get; private set; }
+
+        public string Location { get; private set; }
+
+        public Zoo(string location)
+        {
+            Enclosures = new List<Enclosure>();
+            Employees = new List<IEmployee>();
+            Location = location;
+        }
+
+        public void AddEnclosure(Enclosure enclosure)
+        {
+            Enclosures.Add(enclosure);
+        }
+    }
+}
---
+++ tests/Zoo.Core.Tests/ZooTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Zoo.Core.Tests
+{
+    public class ZooTests
+    {
+        [Fact]
+        public void ShouldCreateZoo()
+        {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            Assert.Empty(zoo.Enclosures);
+            Assert.Empty(zoo.Employees);
+            Assert.False(string.IsNullOrEmpty(zoo.Location));
+        }
+
+        [Fact]
+        public void ShouldAddEnclosure()
+        {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            const string name = "Enclosure 1";
+            const int squareFeet = 10000;
+            var enclosure = new Enclosure(name, zoo, squareFeet);
+            zoo.AddEnclosure(enclosure);
+            Assert.Equal(enclosure, zoo.Enclosures[0]);
+        }
+
+        [Fact]
+        public void ShouldFindAvailableEnclosure()
+        {
+
+        }
+    }
+}
