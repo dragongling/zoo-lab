d833393b237e826116acb3a153ac4ed132fd3b58
Alexander Slesarenko <dragongling@gmail.com>
2021-11-16 15:17:53 +0500

    feat: hiring employees and finding enclosures for animals

--- src/Zoo.Core/Zoo.cs
+++ src/Zoo.Core/Zoo.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zoo.Core.Animals;
 using Zoo.Core.Employees;
+using Zoo.Core.Employees.Hiring;
 
 namespace Zoo.Core
 {
     public class Zoo
     {
+        private readonly HireValidatorProvider validatorProvider = new();
+
         public List<Enclosure> Enclosures { get; private set; }
         public List<IEmployee> Employees { get; private set; }
 
@@ -20,6 +26,42 @@ namespace Zoo.Core
         public void AddEnclosure(Enclosure enclosure)
         {
             Enclosures.Add(enclosure);
+        }
+
+        public void FindAvailableEnclosure(Animal animal)
+        {
+            foreach(var enclosure in Enclosures)
+            {
+                try
+                {
+                    Console.WriteLine(enclosure.Name);
+                    enclosure.AddAnimal(animal);
+                    return;
+                }
+                catch (InvalidOperationException) { }
+            }
+            throw new NoAvailableEnclosureException();
+        }
+
+        public void HireEmployee(IEmployee employee)
+        {
+            var validationErrors = validatorProvider
+                .GetHireValidator(employee)
+                .ValidateEmployee(
+                    employee,
+                    Enclosures.Select(e => e.Animals).SelectMany(a => a).ToList());
+            if (validationErrors.Count == 0)
+                Employees.Add(employee);
+        }
+
+        public void FeedAnimals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HealAnimals()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- tests/Zoo.Core.Tests/ZooTests.cs
+++ tests/Zoo.Core.Tests/ZooTests.cs
@@ -1,4 +1,6 @@
 ﻿using Xunit;
+using Zoo.Core.Animals;
+using Zoo.Core.Employees;
 
 namespace Zoo.Core.Tests
 {
@@ -27,7 +29,39 @@ namespace Zoo.Core.Tests
         [Fact]
         public void ShouldFindAvailableEnclosure()
         {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            var penguin = new Penguin();
 
+            Assert.Throws<NoAvailableEnclosureException>(() => zoo.FindAvailableEnclosure(penguin));
+
+            zoo.AddEnclosure(new Enclosure("Snake terrarium", zoo, 4));
+            var forPenguin = new Enclosure("Penguin chamber", zoo, 20);
+            zoo.AddEnclosure(forPenguin);
+            zoo.FindAvailableEnclosure(penguin);
+            Assert.Equal(penguin, forPenguin.Animals[0]);
+        }
+
+        [Fact]
+        public void ShouldHireEmployees()
+        {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            var zooKeeper = new ZooKeeper("L", "F");
+
+            zoo.HireEmployee(zooKeeper);
+        }
+
+        [Fact]
+        public void ShouldFeedAnimals()
+        {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            zoo.FeedAnimals();
+        }
+
+        [Fact]
+        public void ShouldHealAnimals()
+        {
+            var zoo = new Zoo("49°54′10″N 57°20′0″E");
+            zoo.HealAnimals();
         }
     }
 }
